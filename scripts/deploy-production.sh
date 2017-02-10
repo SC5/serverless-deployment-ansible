1e4335f34484874231bc4554662d5cf487dc57e8
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-02-10 11:16:11 +0200

    pass service version to deploy scripts

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,6 +2,7 @@
 
 # run ansible
 docker run -v $(pwd):/src -w /src serverless-deployment /bin/bash -c "\
+export SERVICE_VERSION=$SERVICE_VERSION && \
 ansible-playbook -vvvv --inventory-file inventories/production/inventory site.yml"
 
 [[ $? -eq 0 ]] || { echo "Build failed!" ; exit 1; }
